793aa3305647631bd0ee8b97e3550c17e90b92ef
Alejandro <gomez@physics.rutgers.edu>
2017-06-09 22:27:23 +0200

    adding file for 2017 data

--- TriggerDevelopment/test/crab3.py
+++ TriggerDevelopment/test/crab3.py
@@ -15,9 +15,10 @@ config.General.requestName = ''
 config.General.workArea = 'crab_projects'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'hlt_B2GTriggers_MC.py'  #'hlt_B2GTriggers_2016Data_new.py'
+#config.JobType.psetName = 'hlt_B2GTriggers_MC.py'  
+config.JobType.psetName = 'hlt_B2GTriggers_2017Data.py'
 config.JobType.allowUndistributedCMSSW = True
-config.JobType.inputFiles = [ '/afs/cern.ch/user/s/sdonato/AFSwork/public/genJetPtHatPU/0.txt' ]
+#config.JobType.inputFiles = [ '/afs/cern.ch/user/s/sdonato/AFSwork/public/genJetPtHatPU/0.txt' ]
 config.General.transferLogs = True
 
 config.Data.inputDataset = ''
@@ -41,8 +42,8 @@ if __name__ == '__main__':
 	from CRABAPI.RawCommand import crabCommand
 
 	Samples = [ 
-		#'/HLTPhysics0/Run2016H-v1/RAW'
-		'/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v2/GEN-SIM-RAW'
+		'/HLTPhysics/Run2017A-v1/RAW',
+		#'/QCD_Pt-15to3000_TuneCUETP8M1_Flat_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v2/GEN-SIM-RAW'
 			]
 
 	
@@ -52,10 +53,10 @@ if __name__ == '__main__':
 		#procName = dataset.split('/')[1]+dataset.split('/')[2]+'_'+version
 		procName = dataset.split('/')[1]+'_'+version
 		#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt'
-		#config.Data.lumiMask = 'json277069_1e34.json'
+		config.Data.lumiMask = '/afs/cern.ch/work/t/tosi/public/STEAM/json/firstCollisions17_v3p1_v3_PS_2e34.json'
 		config.Data.splitting = 'LumiBased'
 		config.Data.unitsPerJob = 1
-		#config.Data.runRange = '283820'
+		#config.Data.runRange = '295612:295614'
 
 		config.General.requestName = procName
 		print config
